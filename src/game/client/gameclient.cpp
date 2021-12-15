323a479754ef894c60efa8f5d3315dd96a1d1e2a
Alexander Akulich <akulichalexander@gmail.com>
2021-12-15 22:50:09 +0300

    Show infected hook for infected classes

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -3921,6 +3921,9 @@ void CGameClient::LoadInfclassSkin(const char *pPath, bool AsDir)
 			}
 		}
 
+		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteHookChain);
+		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteHookHead);
+
 		m_InfclassSkinLoaded = false;
 	}
 
@@ -3963,6 +3966,9 @@ void CGameClient::LoadInfclassSkin(const char *pPath, bool AsDir)
 				}
 			}
 		}
+
+		m_InfclassSkin.m_SpriteHookChain = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_HOOK_CHAIN]);
+		m_InfclassSkin.m_SpriteHookHead = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_HOOK_HEAD]);
 
 		m_InfclassSkinLoaded = true;
 
