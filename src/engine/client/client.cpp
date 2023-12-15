b32784ef6844b99902a51a6c66a45fdec341e05f
Alexander Akulich <akulichalexander@gmail.com>
2023-12-15 19:20:39 +0300

    Introduce InfClass config file

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -4814,6 +4814,9 @@ int main(int argc, const char **argv)
 	pKernel->RegisterInterface(CreateFavorites().release());
 	pKernel->RegisterInterface(CreateGameClient());
 
+	const char aInfclassConfigFile[] = "settings_infclass.cfg";
+	SetInfclassConfigDomainId(pConfigManager->RegisterConfigDomain(aInfclassConfigFile));
+
 	pEngine->Init();
 	pConsole->Init();
 	pConfigManager->Init();
