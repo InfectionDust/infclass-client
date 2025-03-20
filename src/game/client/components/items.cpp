a141b2babefb035eb33b8823ab427f4eb16bcb48
Marvin Winkens <m.winkens@fz-juelich.de>
2025-03-20 19:17:51 +0100

    square up laser door heads
    (cherry picked from commit 0c6c496b30dcb9fa1963f1713706d010372910a8)

--- src/game/client/components/items.cpp
+++ src/game/client/components/items.cpp
@@ -349,6 +349,16 @@ void CItems::RenderLaser(const CLaserData *pCurrent, bool IsPredicted)
 	}
 
 	// render head
+	if(Type == LASERTYPE_DOOR)
+	{
+		Graphics()->TextureClear();
+		Graphics()->QuadsSetRotation(0);
+		Graphics()->SetColor(OuterColor);
+		Graphics()->RenderQuadContainerEx(m_ItemsQuadContainerIndex, m_DoorHeadOffset, 1, Pos.x - 8.0f, Pos.y - 8.0f);
+		Graphics()->SetColor(InnerColor);
+		Graphics()->RenderQuadContainerEx(m_ItemsQuadContainerIndex, m_DoorHeadOffset, 1, Pos.x - 6.0f, Pos.y - 6.0f, 6.f / 8.f, 6.f / 8.f);
+	}
+	else
 	{
 		int CurParticle = (Client()->GameTick(g_Config.m_ClDummy) % 3);
 		Graphics()->TextureSet(GameClient()->m_ParticlesSkin.m_aSpriteParticleSplat[CurParticle]);
@@ -802,6 +812,9 @@ void CItems::OnInit()
 		Graphics()->QuadsSetSubset(0, 0, 1, 1);
 		ParticleSplatOffset = RenderTools()->QuadContainerAddSprite(m_ItemsQuadContainerIndex, 24.f);
 	}
+
+	IGraphics::CQuadItem Brick(0, 0, 16.0f, 16.0f);
+	m_DoorHeadOffset = Graphics()->QuadContainerAddQuads(m_ItemsQuadContainerIndex, &Brick, 1);
 
 	Graphics()->QuadContainerUpload(m_ItemsQuadContainerIndex);
 }
